8f154e5a7072bba07440ecaaad4d8f8c6d789ddf
Evan Kim <evan.e.kim@gmail.com>
2024-08-21 16:52:15 -0700

    added in some projects lol

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,8 +3,7 @@ import ProjectsPage from "./projects";
 export default function Projects() {
   return (
     <main className="w-full flex flex-col items-center p-8">
-      Coming soon...
-      {/* <ProjectsPage /> */}
+      <ProjectsPage />
     </main>
   );
 }
--- src/app/projects/projects.tsx
+++ src/app/projects/projects.tsx
@@ -31,7 +31,7 @@ const ProjectBox: React.FC<{
   demoLink: string;
   tags: string[];
 }> = ({ title, description, githubLink, demoLink, tags }) => (
-  <div className="flex flex-col mb-8 p-4 border rounded-md border-gray-300 dark:border-gray-600 shadow-sm">
+  <div className="flex flex-col mb-1 p-4 border rounded-md border-gray-300 dark:border-gray-600 shadow-sm">
     <div className="flex-grow">
       <div className="flex justify-between items-center">
         <h3 className="text-xl font-semibold text-black dark:text-white">
@@ -73,19 +73,64 @@ const ProjectBox: React.FC<{
 const ProjectsPage = () => {
   const projects = [
     {
-      title: "Project 1",
+      title: "ScGAN",
       description:
-        "A brief description of Project 1. This is where you can elaborate on the technologies used and the purpose of the project.",
-      tags: ["React", "Node.js", "MongoDB"],
-      githubLink: "https://github.com/yourusername/project1",
-      demoLink: "https://demo.project1.com",
+        "Used generative modeling to aid High-temp superconductor discovery; presented at APS March Meeting 2023. Published in the Journal of Physics: Condensed Matter.",
+      tags: ["ML", "Python", "TensorFlow", "Physics"],
+      demoLink: "https://doi.org/10.1088/1361-648X/acfdebA",
     },
     {
-      title: "Project 2",
+      title: "Quadratic Flow",
+      description: "WIP.",
+      tags: ["ML", "Python", "PyTorch", "CV"],
+    },
+    {
+      title: "Intuision",
       description:
-        "Description of Project 2. Highlight the key features and your role in the project.",
-      tags: ["Python", "Machine Learning", "TensorFlow"],
-      githubLink: "https://github.com/yourusername/project2",
+        "Building a new style of learning – imagine interactive scenes in-place of the static diagrams of a standard textbook. Integrated AI features to edit and generate animations, provide chatbot assistance, and save user learning preferences.Description of Project 2. Highlight the key features and your role in the project.",
+      tags: ["React", "MERN", "LLM", "RAG", "Web"],
+      githubLink: "https://github.com/essence-learning/intuision",
+    },
+    {
+      title: "Pigeon",
+      description:
+        "An email client with automated email response generation utilizing RAG. Developed with the HackMIT team to aid the HackMIT logistics team.",
+      tags: ["React", "Flask", "RAG", "Web", "LLM"],
+      githubLink: "https://github.com/techx/pigeon",
+    },
+    {
+      title: "Plume",
+      description:
+        "The all-in-one judging and registration platform for HackMIT 2024. I sepcifically built the discord verification system to allow users to interact with one another on a discord server.",
+      tags: ["React", "Flask", "PostgreSQL", "Discord", "Web"],
+    },
+    {
+      title: "Hexblend",
+      description:
+        "The 1st place winnner of MIT's annual web.lab hackathon: a multiplayer color mixing game.",
+      tags: ["React", "MERN", "Socket.IO", "Web"],
+      demoLink: "https://hexblend.onrender.com/",
+    },
+    {
+      title: "Slipping Hexagons",
+      description:
+        "An educational YouTube Channel which teaches unique physics concepts with a computational animation library (Manim). Over 50k views and 2.5k subscribers.",
+      tags: ["Manim", "Physics", "Graphic Production"],
+      demoLink: "https://youtube.com/@slippinghexagons",
+    },
+    {
+      title: "Piper the Pika",
+      description:
+        "A recreation of the classic game Sonic the Hedgehog with added twists. Built in < 1 month, supporting multiple levels and map imports from an SVG.",
+      tags: ["C#", "Game Design"],
+      githubLink: "https://github.com/evnkim/piper-the-pika",
+    },
+    {
+      title: "evnkim.github.io",
+      description:
+        "This website! Built with Next.js, Tailwind CSS, and Vercel.",
+      tags: ["Next.js", "Tailwind CSS", "Web"],
+      githubLink: "https://github.com/evnkim/evnkim.github.io",
     },
     // Add more projects as needed
   ];
@@ -104,17 +149,19 @@ const ProjectsPage = () => {
 
   return (
     <div className="w-full max-w-3xl mx-auto">
-      <div className="mb-8 flex space-x-2 items-center">
-        <h4>Filter by tag:</h4>
-        {allTags.map((tag) => (
-          <Tag
-            key={tag}
-            name={tag}
-            isButton={true}
-            isSelected={selectedTag === tag}
-            onClick={() => setSelectedTag(tag)}
-          />
-        ))}
+      <div className="mb-8">
+        <h4 className="mb-2">Filter by tag:</h4>
+        <div className="flex flex-wrap gap-2">
+          {allTags.map((tag) => (
+            <Tag
+              key={tag}
+              name={tag}
+              isButton={true}
+              isSelected={selectedTag === tag}
+              onClick={() => setSelectedTag(tag)}
+            />
+          ))}
+        </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         {filteredProjects.map((project, index) => (
